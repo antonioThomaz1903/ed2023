dfd1ce99484a6c19e10e68c2fe65f94ce512f823
edp facom <edpfacom@gmail.com>
2023-03-21 17:24:52 -0400

    apenas um teste

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(void){
     int i;
     for(i=0;i<10;i++){
         if (e_par(i)) printf("%d par\n",i);
-        else printf("%d impar\n",i);
+        else printf("%d  impar\n",i);
     }
     return EXIT_SUCCESS;
 }
