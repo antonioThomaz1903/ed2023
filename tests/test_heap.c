4c9aa0a4b2c53095ace866ffa29f1f3ad107206a
edp facom <edpfacom@gmail.com>
2023-03-28 16:25:23 -0400

    implementado pai resulve #1

--- tests/test_heap.c
+++ tests/test_heap.c
@@ -7,6 +7,13 @@
 void test_pai(){
     assert(heap_pai(0)==0);
     assert(heap_pai(1)==0);
+    assert(heap_pai(2)==0);
+    assert(heap_pai(3)==1);
+    assert(heap_pai(4)==1);
+    assert(heap_pai(5)==2);
+    assert(heap_pai(6)==2);
+    assert(heap_pai(7)==3);
+    assert(heap_pai(8)==3);
 }
 
 
