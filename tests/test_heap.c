b70935957386270e0fc6b76898d14ffa24515ef0
Edson Takashi Matsubara <edson.matsubara@ufms.br>
2023-04-04 13:44:11 -0400

    teste do desce

--- tests/test_heap.c
+++ tests/test_heap.c
@@ -28,9 +28,33 @@ void test_filhos(){
 
 }
 
+void test_desce(){
+    /* inicialização*/
+    int n = 10;
+    int v[10] = {26,7,14,16,8,9,11,4,12,6};
+
+    /* execução */
+    int i = 1;
+    heap_desce(v,n,i);
+    
+    /* teste*/
+    assert(v[1] == 16);
+    assert(v[3] == 12);
+    assert(v[8] == 7);
+    
+     
+    heap_desce(v,n,4);
+    assert(v[4] == 8);
+    assert(v[9] == 6);
+
+    heap_desce(v,n,6);
+    assert(v[6] == 11);
+}
+
 
 
 int main(void){
     test_pai();
+    test_filhos();
     return EXIT_SUCCESS;
 }
