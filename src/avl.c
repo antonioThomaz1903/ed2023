0ac6d83c3ca39c90f81a577d9df5d793f98048e0
Edson Takashi Matsubara <edson.matsubara@ufms.br>
2023-05-09 13:37:48 -0400

    rotacoes implementadas

--- src/avl.c
+++ src/avl.c
@@ -33,3 +33,32 @@ void avl_insere(tnode ** parv,titem item){
     (*parv)->h = max(altura((*parv)->esq),altura((*parv)->dir)) + 1;
     /* rebalancear(parv);*/
 }
+
+void RD(tnode **parv){
+    tnode * y = *parv; 
+    tnode * x = y->esq;
+    tnode * A = x->esq;
+    tnode * B = x->dir;
+    tnode * C = y->dir;
+
+    y->esq = B; 
+    x->dir = y;
+    *parv  = x;
+    y->h = max(altura(B),altura(C)) + 1;
+    x->h = max(altura(A),altura(y)) + 1;
+}
+
+void RE(tnode **parv){
+    tnode * x = *parv; 
+    tnode * y = x->dir;
+    tnode * A = x->esq;
+    tnode * B = y->esq;
+    tnode * C = y->dir;
+
+    x->dir = B;
+    y->esq = x; 
+    *parv  = y;
+    x->h = max(altura(A),altura(B)) + 1;
+    y->h = max(altura(x),altura(C)) + 1;
+}
+
