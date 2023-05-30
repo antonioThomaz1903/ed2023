64d25993e315469d82e3b50a82080ba8442247f8
Edson Takashi Matsubara <edson.matsubara@ufms.br>
2023-05-30 14:22:52 -0400

    exemplo de ponteiro para void

--- tests/test_avlg.c
+++ tests/test_avlg.c
@@ -6,6 +6,7 @@ typedef struct _aluno{
     int rga;
     char nome[30];
     char sobrenome[30];
+    int idade;
 }taluno;
 
 typedef struct _node{
@@ -21,10 +22,25 @@ typedef struct _arv{
 }tarv;
 
 int compara_rga(const void *a, const void *b){
-    return 1;
+    taluno * a1;
+    taluno * a2;
+    a1 = (taluno *) a;
+    a2 = (taluno *) b;
+    return a1->rga - a2->rga;
+    //return (*((taluno *)a)).rga - (*((taluno *)b)).rga;
+}
+int compara_idade(const void *a, const void *b){
+    taluno * a1;
+    taluno * a2;
+    a1 = (taluno *) a;
+    a2 = (taluno *) b;
+    return a1->idade - a2->idade;
+    //return (*((taluno *)a)).rga - (*((taluno *)b)).rga;
 }
 
 void avlg_constroi(tarv * parv, int (*compara)(const void *a, const void *b)){
+    parv->raiz = NULL;
+    parv->compara = compara; 
 }
 
 
@@ -38,8 +54,19 @@ void test_compara(){
     a2.rga = 20;
     assert(arv.compara(&a1,&a2)==-10);
 }
+void test_compara_idade(){
+    tarv arv;
+    taluno a1;
+    taluno a2;
+    avlg_constroi(&arv,compara_rga);
+    a1.idade = 10;
+    a2.idade = 20;
+    assert(arv.compara(&a1,&a2)==-10);
+}
+
 
 int main(void){
     test_compara();
+    test_compara_idade();
     return EXIT_SUCCESS;
 }
